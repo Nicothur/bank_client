53edba8afd377c21561fba460a37d193819cda0c
Nicolas Gori <johng117@orange.fr>
2019-05-13 10:02:35 +0200

    Update UserService.ts
    Suppression httpClient

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -1,11 +1,10 @@
 import { User } from '../beans/User';
 import { Transaction } from '../beans/Transaction';
-import { HttpClient } from 'selenium-webdriver/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class UserService {
-    constructor(private http: HttpClient) {
+    constructor() {
         
     }
 
